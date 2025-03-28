1689c2abcaa8d9110a4569da8aac00567bb0a3cf
suma <sumahm03@gmail.com>
2025-03-29 01:20:03 +0530

    updated Day2 file

--- src/abc/Day2.java
+++ src/abc/Day2.java
@@ -1,44 +1,68 @@
 package abc;
 
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Day2 {
-	@AfterClass
-	public void AftClass()
-	{
-		System.out.println("execute After this day2 class");
-	}
 
-	@Parameters({"Link"})
-	@Test()
-	public void SencondDay(String uname)
-	{
-		System.out.println("Day2 Bye");
-		System.out.println(uname);
-	}
+//defined in tesngannotation.xml
 	
+	@Test
+	public void TestCase1() {
+		System.out.println("Execute TC1");
+	}
+	@Test
+	public void TestCase2() {
+		System.out.println("execute TC2");
+	}
 	@BeforeMethod
-	public void BeforeMeth()
-	{
-		System.out.println("execute before every method in day2 class");
+	public void beforeMethod() {
+		System.out.println("execute before each method");
 	}
-	
 	@AfterMethod
-	public void AfterMeth()
-	{
-		System.out.println("execute after every method in day2 class");
+	public void afterMethod() {
+		System.out.println("execute after every method");
 	}
 	@BeforeClass
-	public void BefClass()
-	{
-		System.out.println("execute before this day2 class");
+	public void beforeThisClass() {
+		System.out.println("execute before this class");
 	}
+	@BeforeTest
+	public void beforeThisTest() {
+		System.out.println("execute before this test");
+	}
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("execute before suite");
+	}
+	
+// include and exclude will use to execute or don't execute the methods 
+	@Test
+	public void forinclude() {
+		System.out.println("include this");
+	}
+	@Test
+	public void forexclude() {
+		System.out.println("exclude this");
+	}
+	
+	@Test
+	public void forinclude1() {
+		System.out.println("include this 1");
+	}
+	@Test
+	public void forexclude1() {
+		System.out.println("exclude this 1");
+	}
+	
+	
 
+	
+
+	
+	
 }
-
